b04c6b2c226e47d50a9e4fc2c1cf237b013ca085
Bonarerijt <prudyjj4@gmail.com>
2026-02-05 20:59:17 +0300

    parcel status model added

--- backend/app/models/parcel.py
+++ backend/app/models/parcel.py
@@ -2,3 +2,10 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, Foreig
 from sqlalchemy.sql import func
 from app.database.database import Base
 import enum
+
+
+class ParcelStatus(enum.Enum):
+    pending = "pending"
+    in_transit = "in_transit" 
+    delivered = "delivered"
+    cancelled = "cancelled"
