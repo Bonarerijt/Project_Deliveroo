ea34075ca869add2182fcf3d4c7fbf201c7aa0a7
Bonarerijt <prudyjj4@gmail.com>
2026-02-06 08:27:00 +0300

    test backend authentication

---
+++ test_backend.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+# Test backend authentication
+def test_auth():
+    base_url = "http://localhost:8000"
+    
+    print("Testing Deliveroo Backend Authentication...")
+
+    
